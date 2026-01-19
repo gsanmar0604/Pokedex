c83f77d7a3ff7e6b55991a8471fb4b04e18af1b1
gsanmar0604 <gsanmar0604@g.educaand.es>
2026-01-19 12:59:07 +0100

    Agregar archivos de proyecto.

---
+++ Pokedex/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Pokedex")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Pokedex.Pages")]
