e16eb98a2b3808fbcc51306d83cdfcb5b0829c1c
gsanmar0604 <gsanmar0604@g.educaand.es>
2026-01-19 13:03:49 +0100

    Comprobación I

---
+++ Pokedex/View/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+namespace Pokedex.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (BindingContext is MainViewModel vm)
+        {
+            await vm.LoadPokemonsCommand.ExecuteAsync(null);
+        }
+    }
+}
