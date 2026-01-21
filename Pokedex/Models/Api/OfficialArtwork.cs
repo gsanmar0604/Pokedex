cfc32a13ab166f91d038e1e4098d1bbed437b962
gsanmar0604 <gsanmar0604@g.educaand.es>
2026-01-21 13:32:39 +0100

    bug solucionado - imágenes no cargan

--- Pokedex/Models/Api/OfficialArtwork.cs
+++ Pokedex/Models/Api/OfficialArtwork.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Pokedex.Models.Api
 {
     public class OfficialArtwork
     {
+        [JsonPropertyName("front_default")]
         public string? Front_Default { get; set; }
+        [JsonPropertyName("front_shiny")]
         public string? Front_Shiny { get; set; }
     }
 }
--- Pokedex/Models/Api/PokemonSprite.cs
+++ Pokedex/Models/Api/PokemonSprite.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Pokedex.Models.Api
 {
     public class PokemonSprites
     {
+        [JsonPropertyName("front_default")]
         public string? Front_Default { get; set; }
+        [JsonPropertyName("front_shiny")]
         public string? Front_Shiny { get; set; }
+        [JsonPropertyName("back_default")]
         public string? Back_Default { get; set; }
         public PokemonSpritesOther? Other { get; set; }
     }
